81d4c5d4c942d7b59193d37709821ffc402390b9
Pablo592 <pgaido524@alumnos.iua.edu.ar>
2022-10-13 19:18:06 -0300

    empezando el header
    Signed-off-by: Pablo592 <pgaido524@alumnos.iua.edu.ar>

--- TecnoMoviles/app/src/main/java/com/tecno/tecnomoviles/LoginActivity.kt
+++ TecnoMoviles/app/src/main/java/com/tecno/tecnomoviles/LoginActivity.kt
@@ -2,12 +2,14 @@ package com.tecno.tecnomoviles
 
 import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 
 class LoginActivity: AppCompatActivity() {
 
     private lateinit var registerButton: TextView
+    private lateinit var loginButton: Button
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -16,12 +18,17 @@ class LoginActivity: AppCompatActivity() {
         supportActionBar?.hide()
 
         registerButton = findViewById(R.id.registerButton)
+        loginButton = findViewById(R.id.loginButton)
 
         val registerIntent = Intent(this, RegisterActivity::class.java)
-
+        val loginIntent = Intent(this, HomeActivity::class.java)
 
         registerButton.setOnClickListener(){
             startActivity(registerIntent)
+        }
+
+        loginButton.setOnClickListener(){
+            startActivity(loginIntent)
         }
 
     }
