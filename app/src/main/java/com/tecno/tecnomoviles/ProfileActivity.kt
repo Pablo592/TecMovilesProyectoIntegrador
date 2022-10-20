d2de996f2a44f191896acad466585531efcf4c40
Pablo592 <pgaido524@alumnos.iua.edu.ar>
2022-10-20 15:02:35 -0300

    perfil terminado con header

--- app/src/main/java/com/tecno/tecnomoviles/ProfileActivity.kt
+++ app/src/main/java/com/tecno/tecnomoviles/ProfileActivity.kt
@@ -3,22 +3,32 @@ package com.tecno.tecnomoviles
 import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.add
+import androidx.fragment.app.commit
 import com.tecno.tecnomoviles.databinding.ProfileBinding
+import utils.fragments.HeaderFragment
 
 class ProfileActivity : AppCompatActivity() {
 
-    private lateinit var binding : ProfileBinding
+    private lateinit var binding: ProfileBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.profile)
         supportActionBar?.hide()
 
-        binding = ProfileBinding.inflate(layoutInflater)
-        setContentView(binding.root)
+        if (savedInstanceState == null) {
+            supportFragmentManager.commit {
+                setReorderingAllowed(true)
+                add<HeaderFragment>(R.id.fragment_header)
+            }
 
-        binding.editDataButton.setOnClickListener(){
-            startActivity(Intent(this, EditDataUser::class.java))
+            binding = ProfileBinding.inflate(layoutInflater)
+            setContentView(binding.root)
+
+            binding.editDataButton.setOnClickListener() {
+                startActivity(Intent(this, EditDataUser::class.java))
+            }
         }
     }
 }
