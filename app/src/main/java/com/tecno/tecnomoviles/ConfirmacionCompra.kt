a85982df25c7a527ac85239f7a71c9d0146cc004
Pablo592 <pgaido524@alumnos.iua.edu.ar>
2022-10-21 11:15:25 -0300

    lista la pantalla de la ubicacion y arreglados los botones de cancelar y aceptar cambios en la modificacion de datos

---
+++ app/src/main/java/com/tecno/tecnomoviles/ConfirmacionCompra.kt
@@ -0,0 +1,16 @@
+package com.tecno.tecnomoviles
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class ConfirmacionCompra  : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.confirmacion_compra)
+        supportActionBar?.hide()
+
+
+
+    }
+}
