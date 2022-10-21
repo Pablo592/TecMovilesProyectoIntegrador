f0df8e47a3f4540076964be9a022f672199afb05
Pablo592 <pgaido524@alumnos.iua.edu.ar>
2022-10-21 19:11:54 -0300

    empezando a armar el navigation drower

--- app/src/main/java/utils/fragments/HeaderFragment.kt
+++ app/src/main/java/utils/fragments/HeaderFragment.kt
@@ -1,10 +1,13 @@
 package utils.fragments
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.tecno.tecnomoviles.HomeActivity
+import com.tecno.tecnomoviles.NavigationDrawer
 import com.tecno.tecnomoviles.databinding.HeaderFragmentBinding
 
 class HeaderFragment : Fragment(){
@@ -19,7 +22,6 @@ private  var bindig: HeaderFragmentBinding? = null
         bindig = HeaderFragmentBinding.inflate(inflater)
         return bindig?.root
     }
-
     override fun onDestroy() {
         bindig = null
         super.onDestroy()
