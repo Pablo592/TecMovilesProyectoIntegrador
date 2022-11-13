3b37a05744099889a1e5aac102bfdf0d0dd06786
Pablo592 <pgaido524@alumnos.iua.edu.ar>
2022-11-13 18:42:53 -0300

    Login mejorado
    Signed-off-by: Pablo592 <pgaido524@alumnos.iua.edu.ar>

--- app/src/main/java/com/tecno/tecnomoviles/RegisterActivity.kt
+++ app/src/main/java/com/tecno/tecnomoviles/RegisterActivity.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import android.os.Bundle
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.MutableLiveData
 import com.MyApplication
 import com.tecno.tecnomoviles.databinding.RegisterUserBinding
 import kotlinx.coroutines.Dispatchers
@@ -14,6 +15,8 @@ import persistence.entitys.user.User
 class RegisterActivity: AppCompatActivity() {
 
     private lateinit var binding : RegisterUserBinding
+    val userLiveData = MutableLiveData<User>()
+    var existente: Boolean = false
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -45,11 +48,44 @@ class RegisterActivity: AppCompatActivity() {
                     Toast.makeText(baseContext,"Ingrese su contraseña", Toast.LENGTH_LONG).show()
                     return@setOnClickListener}
             }
-            saveUser()
-            startActivity(Intent(this, LoginActivity::class.java))
+            getProfileForDatabase()
+
+            if(existente){
+                saveUser()
+                startActivity(Intent(this, LoginActivity::class.java))
+            }
+
+        }
+
+        binding.textoCancelar.setOnClickListener(){
+            startActivity(Intent(this, LoginActivity::class.java))º
         }
 
     }
+
+    private fun getProfileForDatabase() {
+        runBlocking {
+            launch {
+                if (MyApplication.preferences.getUserName()
+                        .isNotEmpty() || MyApplication.preferences.getUserName().isNotBlank()) {
+                    if (MyApplication.myAppDatabase.userDao()
+                            .isEmpty(binding.userInput.text.toString()) > 0
+                    ) {
+                        Toast.makeText(
+                            baseContext,
+                            "El usuario " + binding.userInput.text.toString() + " ya existe",
+                            Toast.LENGTH_LONG
+                        ).show()
+                        existente = false
+                    }else{
+                        existente = true
+                    }
+                }
+            }
+        }
+    }
+
+
 
     private fun saveUser(){
         runBlocking {
